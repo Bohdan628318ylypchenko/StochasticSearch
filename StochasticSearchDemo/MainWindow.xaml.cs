716356e4cb155825bcff4c30315f2e6e6ea66c9a
Bohdan Pylypchenko <Bohdan314159yLypchenko@outlook.com>
2023-06-26 03:02:47 +0300

    Finishing UI

---
+++ StochasticSearchDemo/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using MathNet.Numerics.LinearAlgebra.Double;
+using StochasticSearchLib;
+using StochasticSearchLib.Optimization1D;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Windows.Input;
+using ScottPlot;
+
+namespace StochasticSearchDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _isGoldenCut = false;
+        private bool _isBounded = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Func<double, double, double> CreateFunctionXYFromString(string fSrc)
+        {
+            var str = "(x, y) => " + fSrc;
+            var options = ScriptOptions.Default.AddImports(new string[] { "System" });
+            return CSharpScript.EvaluateAsync<Func<double, double, double>>(str, options).Result;
+        }
+
+        public Func<T, double> CreateFunctionXFromString<T>(string fSrc)
+        {
+            var str = "(x) => " + fSrc;
+            var options = ScriptOptions.Default.AddImports(new string[] { "System" });
+            return CSharpScript.EvaluateAsync<Func<T, double>>(str, options).Result;
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait; 
+
+            Plot.Plot.Clear();
+
+            Func<MathNet.Numerics.LinearAlgebra.Double.Vector, double> f;
+            MathNet.Numerics.LinearAlgebra.Double.Vector x0;
+            ISearcher1D searcher1D;
+            double goldenCutEpsilon, goldenCutSvenDelta;
+            int seed;
+            int directionGenerationAttemptBeforeRadiusDecrementCount;
+            double rMin, rInit, rD;
+            Func<double, double> penalty;
+            int penaltyCoefCount;
+            Func<int, double> penaltyCoefFunction;
+            double[] penaltyCoefficients;
+            MathNet.Numerics.LinearAlgebra.Double.Vector[] bounds;
+            StochasticSearcher stochasticSearcher;
+            MathNet.Numerics.LinearAlgebra.Double.Vector[] result;
+            try
+            {
+                var tf = CreateFunctionXYFromString(FunctionTextBox.Text);
+                f = v => tf(v[0], v[1]);    
+                var x0s = StartPointTextBox.Text.Split(' ');
+                x0 = DenseVector.OfArray(new double[] { Double.Parse(x0s[0]),
+                                                        Double.Parse(x0s[1] )});
+                if (_isGoldenCut)
+                {
+                    goldenCutEpsilon = Double.Parse(GCEpsilonTextBox.Text);
+                    goldenCutSvenDelta = Double.Parse(GCSvenDeltaTextBox.Text);
+                    searcher1D = new GoldenCutSearcher1D(goldenCutEpsilon, goldenCutSvenDelta);
+                }
+                else
+                {
+                    searcher1D = new NaiveSearcher1D();
+                }
+
+                seed = Int32.Parse(SeedTextBox.Text);
+                directionGenerationAttemptBeforeRadiusDecrementCount = Int32.Parse(DGACountTextBox.Text);
+                rMin = double.Parse(RMinTextBox.Text);
+                rInit = double.Parse(RInitTextBox.Text);
+                rD = double.Parse(RdTextBox.Text);
+
+                stochasticSearcher = new StochasticSearcher(searcher1D, seed);
+
+                if (_isBounded)
+                {
+                    penalty = CreateFunctionXFromString<double>(PenaltyTextBox.Text);
+                    penaltyCoefCount = Int32.Parse(PenaltyCoefCountTextBox.Text);
+                    penaltyCoefFunction = CreateFunctionXFromString<Int32>(PenaltyCoefFunctionTextBox.Text);
+                    penaltyCoefficients = Enumerable.Range(1, penaltyCoefCount)
+                                                    .Select(penaltyCoefFunction)
+                                                    .ToArray();
+                    var lines = BoundsTextBox.Text.Split('\n');
+                    bounds = new MathNet.Numerics.LinearAlgebra.Double.Vector[lines.Length];
+                    for (var i = 0; i < lines.Length; i++)
+                    {
+                        var coords = lines[i].Split(' ');
+                        var x = Double.Parse(coords[0]);
+                        var y = Double.Parse(coords[1]);
+                        bounds[i] = DenseVector.OfArray(new double[] { x, y });
+                    }
+                    result = stochasticSearcher.MinimizeFunctionInBounds(f,
+                                                                         bounds, penalty, penaltyCoefficients,
+                                                                         x0, directionGenerationAttemptBeforeRadiusDecrementCount,
+                                                                         rMin, rInit, rD);
+                    double[] bX = bounds.Select(v => v[0]).ToArray();
+                    double[] bY = bounds.Select(v => v[1]).ToArray();
+                    Plot.Plot.AddPolygon(bX, bY, Plot.Plot.GetNextColor(.7));
+                }
+                else
+                {
+                    result = stochasticSearcher.MinimizeFunction(f,
+                                                                 x0,
+                                                                 directionGenerationAttemptBeforeRadiusDecrementCount,
+                                                                 rMin, rInit, rD);
+                }
+                double[] rX = result.Select(v => v[0]).ToArray();
+                double[] rY = result.Select(v => v[1]).ToArray();
+
+                ResultTextBox.Text = "";
+                for (var i = 0; i < result.Length; i++)
+                {
+                    ResultTextBox.Text += rX[i] + " " + rY[i] + '\n';
+                }
+                ResultTextBox.Text += "Step count: " + result.Length;
+
+                Plot.Plot.AddScatter(rX, rY);
+
+                Plot.Plot.AddHorizontalLine(0);
+                Plot.Plot.AddVerticalLine(0);
+
+                Plot.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input!", "Oops!");
+            }
+
+            Mouse.OverrideCursor = null; 
+        }
+
+        private void GoldenCutRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            _isGoldenCut = !_isGoldenCut;
+            GCEpsilonTextBox.IsEnabled = _isGoldenCut;
+            GCSvenDeltaTextBox.IsEnabled = _isGoldenCut;
+            GoldenCutRadioButton.IsChecked = _isGoldenCut;
+        }
+
+        private void BoundsRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            _isBounded = !_isBounded;
+            PenaltyTextBox.IsEnabled = _isBounded;
+            PenaltyCoefCountTextBox.IsEnabled = _isBounded;
+            PenaltyCoefFunctionTextBox.IsEnabled = _isBounded;
+            BoundsTextBox.IsEnabled = _isBounded;
+            BoundsRadioButton.IsChecked = _isBounded;
+        }
+    }
+}
